32b5ffd54203cca0a79800fba284189228b39627
liuxinghao <liuxinghao@renyz.com>
2017-03-15 18:22:31 +0800

    修改验证消息

--- Weixin/Home/Controller/IndexController.class.php
+++ Weixin/Home/Controller/IndexController.class.php
@@ -3,6 +3,61 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+       // 1.讲timestamp，nonce，token按字典排序
+		$timestamp 	= $_GET['timestamp'];
+		$nonce		= $_GET['nonce'];
+		$token		= 'dacaozhuang';
+		$signature	= $_GET['signature'];
+		$array 		= array($timestamp,$nonce,$token);
+		sort($array);
+		// 2.将排序后的三个参数拼接之后用sha1加密
+		$tmpstr 	= implode('',$array);//join
+		$tmpstr		= sha1($tmpstr);
+		// 3.将加密后的字符串与signature进行对比，判断该请求是否是来自微信
+		if($tmpstr == $signature){
+			echo $_GET['echostr'];
+			exit;
+		}else{
+			$this->reponsemsg();
+		}
+    }
+    // 接受时间推送并回复
+    public function reponsemsg(){
+    	// 1.获取到微信推送过来的post数据（xml格式）
+    	$postarr = $GLOBALS['HTTP_RAW_POST_DATA'];
+    	//2.处理获取到的数据
+		// <xml>
+		// <ToUserName><![CDATA[toUser]]></ToUserName>
+		// <FromUserName><![CDATA[FromUser]]></FromUserName>
+		// <CreateTime>123456789</CreateTime>
+		// <MsgType><![CDATA[event]]></MsgType>
+		// <Event><![CDATA[subscribe]]></Event>
+		// </xml>
+    	$postobj = simplexml_load_string($postarr);
+    	// $postobj->ToUserName = '';
+    	// $postobj->FromUserName = '';
+    	// $postobj->CreateTime = '';
+    	// $postobj->MsgType = '';
+    	// $postobj->Event = '';
+    	if(strtolower($postobj->MsgType) == 'event'){
+    		// 是否是关注
+    		if (strtolower($postobj->Event) == 'subscribe') {
+    			//回复用户消息
+    			$toUser 	= $postobj->FromUserName;
+    			$fromUser 	= $postobj->ToUserName;
+    			$time		= time();
+    			$MsgType	= 'text';
+    			$content	= '欢迎加入大家庭';
+    			$template = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							</xml>";
+				$info = sprintf($template,$toUser,$fromUser,$time,$MsgType,$content);
+				echo $info;
+    		}
+    	}
     }
 }
